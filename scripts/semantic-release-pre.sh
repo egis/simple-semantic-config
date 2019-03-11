3664f5d0a8d7730777d51eb3d182a6aba14994f1
Artem Vasiliev <artem.job@gmail.com>
2019-03-12 00:22:06 +0300

    chore: pass correct branch
    ..for better error messages

--- scripts/semantic-release-pre.sh
+++ scripts/semantic-release-pre.sh
@@ -5,7 +5,7 @@ if [[ "$OUT_DIR" == "" ]]; then
 fi
 
 mkdir $OUT_DIR
-CIRCLE_PR_NUMBER="" CIRCLE_PR_REPONAME="" CIRCLE_PR_USERNAME="" CIRCLE_PULL_REQUEST="" CIRCLE_PULL_REQUESTS="" CI_PULL_REQUEST="" CI_PULL_REQUESTS="" CIRCLE_BRANCH="master" yarn semantic-release --dry-run > $OUT_DIR/semantic-dry.out || true
+CIRCLE_PR_NUMBER="" CIRCLE_PR_REPONAME="" CIRCLE_PR_USERNAME="" CIRCLE_PULL_REQUEST="" CIRCLE_PULL_REQUESTS="" CI_PULL_REQUEST="" CI_PULL_REQUESTS="" yarn semantic-release --dry-run --branch $CIRCLE_BRANCH > $OUT_DIR/semantic-dry.out || true
 cat $OUT_DIR/semantic-dry.out
 perl -ne 'print "$1\n" if /The next release version is (.*)$/' $OUT_DIR/semantic-dry.out > $OUT_DIR/.version
 VERSION=$(cat $OUT_DIR/.version)
