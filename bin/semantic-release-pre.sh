65734847694f565371ea0a880b7b3e50049c2608
Artem Vasiliev <artem.job@gmail.com>
2020-11-18 21:50:30 +0300

    feat: make version file configurable, too

--- bin/semantic-release-pre.sh
+++ bin/semantic-release-pre.sh
@@ -3,13 +3,16 @@
 if [[ "$OUT_DIR" == "" ]]; then
   OUT_DIR=build
 fi
+if [[ "$OUT_FILE" == "" ]]; then
+  OUT_FILE=version
+fi
 
 mkdir $OUT_DIR
 if [[ "${BRANCH}" == "" ]]; then export BRANCH=${CIRCLE_BRANCH}; fi
 CIRCLE_PR_NUMBER="" CIRCLE_PR_REPONAME="" CIRCLE_PR_USERNAME="" CIRCLE_PULL_REQUEST="" CIRCLE_PULL_REQUESTS="" CI_PULL_REQUEST="" CI_PULL_REQUESTS="" yarn semantic-release --dry-run --branches $BRANCH > $OUT_DIR/semantic-dry.out || true
 cat $OUT_DIR/semantic-dry.out
 perl -ne 'print "$1\n" if /next release version is (.*)$/' $OUT_DIR/semantic-dry.out > $OUT_DIR/.version
-VERSION=$(cat $OUT_DIR/.version)
+VERSION=$(cat $OUT_DIR/$OUT_FILE)
 if [ "${VERSION}" == "" ]; then
     echo "Semantic version isn't generated - please investigate"
     if [ "${ALLOW_FAILING}" == "false" ]; then
@@ -18,4 +21,4 @@ if [ "${VERSION}" == "" ]; then
       echo "..but we allow failing"
     fi
 fi
-cat $OUT_DIR/.version
+cat $OUT_DIR/$OUT_FILE
